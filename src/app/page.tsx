1c3296ddaec1f8164d5d0988307bdb64f3e749a2
whetherington-tech <Will@hetheringtonteam.com>
2025-09-24 16:23:31 -0500

    Remove background for iframe embedding - let HubSpot handle background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import WaterQualityCalculatorNew from '@/components/WaterQualityCalculatorNew'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-12">
-      <div className="max-w-6xl mx-auto space-y-8">
+    <div className="min-h-screen">
+      <div className="max-w-6xl mx-auto">
         <WaterQualityCalculatorNew />
       </div>
     </div>
